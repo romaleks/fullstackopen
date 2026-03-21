1df2b16eb4b929e36e6e4ce31be242f0021fe8f0
romaleks <romanov1371@gmail.com>
2026-03-21 21:30:40 +0300

    Finish the seventh part 'Routed Anecdotes' application

---
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+export const useField = (name) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  const reset = () => {
+    setValue('')
+  }
+
+  return {
+    props: {
+      name,
+      value,
+      onChange,
+    },
+    reset,
+  }
+}
